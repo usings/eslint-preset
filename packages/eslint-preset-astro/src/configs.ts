824b008ecc718240e009422afa51151e8ec048b8
Joie <leaps@live.com>
2023-10-04 22:24:24 +0800

    feat: migrate to astro recommended rules

--- packages/eslint-preset-astro/src/configs.ts
+++ packages/eslint-preset-astro/src/configs.ts
@@ -24,17 +24,7 @@ export const astro = ({ enableTs = true, overrides = {} }: OptionsAstro = {}): F
       },
       processor: pluginAstro.processors!['.astro'],
       rules: {
-        'astro/no-conflict-set-directives': 'error',
-        'astro/no-deprecated-astro-canonicalurl': 'error',
-        'astro/no-deprecated-astro-fetchcontent': 'error',
-        'astro/no-deprecated-astro-resolve': 'error',
-        'astro/no-deprecated-getentrybyslug': 'error',
-        'astro/no-set-text-directive': 'error',
-        'astro/no-unused-define-vars-in-style': 'error',
-        'astro/prefer-class-list-directive': 'error',
-        'astro/prefer-object-class-list': 'error',
-        'astro/prefer-split-class-list': 'error',
-        'astro/valid-compile': 'error',
+        ...(pluginAstro as any).configs['recommended'].rules,
 
         ...overrides,
       },
