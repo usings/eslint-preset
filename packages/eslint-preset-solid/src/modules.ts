d0394695d6e099fd83e0fef0b5673f246f043ce8
Joie <leaps@live.com>
2023-11-02 14:00:24 +0800

    fix: remove unused eslint-disable directive

--- packages/eslint-preset-solid/src/modules.ts
+++ packages/eslint-preset-solid/src/modules.ts
@@ -1,5 +1,4 @@
 /* eslint-disable import/newline-after-import */
-/* eslint-disable import/first */
 
 import type { ESLintPlugin } from 'eslint-preset-basic'
 import { interopDefault } from 'eslint-preset-basic/modules'
